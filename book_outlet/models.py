b20c7822fe982b487e6182635d1b5f11b92cee8b
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2023-12-16 13:14:16 -0600

    migrations and models

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# django.db.models.Model contains all the sql needed to create models
+class Book(models.Model):
+    # django auto-creates the primary ID
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField()
+
+    
