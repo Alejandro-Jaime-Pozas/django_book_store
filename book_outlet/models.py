c361f7e7551ea389406de4158756b349221b12b6
Alex Jaime <ajaimepozas@gmail.com>
2023-12-18 16:12:51 -0600

    added basic book model, templates

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -6,6 +7,9 @@ from django.db import models
 class Book(models.Model):
     # django auto-creates the primary ID
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)]) # to only allow certain values
+    author = models.CharField(max_length=100, null=True)
+    is_bestselling = models.BooleanField(default=False)
 
-    
+    def __str__(self) -> str:
+        return f"{self.title} - by {self.author} ({self.rating})"
