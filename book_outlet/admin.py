942d3df334bee28ecb60404b692eac79c94b4405
Alex Jaime <ajaimepozas@gmail.com>
2023-12-19 12:17:13 -0600

    customize admin site views

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Book
+
 # Register your models here.
+
+# this class added manually
+# this class allows for modification of django admin site
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ('slug',) # must include a tuple or list for read-only fields
+    prepopulated_fields = {'slug': ('title', )} # allows for real-time text updates
+    list_filter = ('author', 'rating', ) # this allows for filtering on admin site
+    list_display = ('title', 'author', ) # allows for columns when displaying objects
+
+admin.site.register(Book, BookAdmin) # this tells Django to ADD this model to the admin site, and that BookAdmin class is linked to the Book class
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -13,15 +13,17 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)]) # to only allow certain values
     author = models.CharField(max_length=100, null=True)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True) # create slug format like lord-of-the-rings; can improve field searches by setting index to True (db_index=True)
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True) # create slug format like lord-of-the-rings; can improve field searches by setting index to True (db_index=True); editable=False does not allow admin user to edit this field
 
     # allows to get a url that represents each instance of this Book model
     def get_absolute_url(self):
         return reverse('book-detail', args=[self.slug]) # pass id into args
     
-    def save(self, *args, **kwargs) -> None:
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs) # fwd all args and kwargs from new method to original save method
+    # # don't need this below since we're already in admin.py securely adding a slug value and accounting for it being a valid value
+    # # this below save() overrides the original method, so always include params from original
+    # def save(self, *args, **kwargs) -> None:
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs) # fwd all args and kwargs from new method to original save method
 
     def __str__(self) -> str:
         return f"{self.title} - by {self.author} ({self.rating})"
