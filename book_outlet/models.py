93bacd085f62885caaca063e338f25e009c7b4a5
Alex Jaime <ajaimepozas@gmail.com>
2023-12-18 18:13:25 -0600

    aggregate values for database items

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,8 @@
+from collections.abc import Iterable
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -10,6 +13,15 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)]) # to only allow certain values
     author = models.CharField(max_length=100, null=True)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True) # create slug format like lord-of-the-rings; can improve field searches by setting index to True (db_index=True)
+
+    # allows to get a url that represents each instance of this Book model
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[self.slug]) # pass id into args
+    
+    def save(self, *args, **kwargs) -> None:
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs) # fwd all args and kwargs from new method to original save method
 
     def __str__(self) -> str:
         return f"{self.title} - by {self.author} ({self.rating})"
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.db.models import Avg #, Max, Min, Sum, Count 
 
 from .models import Book 
 
 # Create your views here.
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by('title') # can order by any field, '-' does desc order vs asc order
+    num_books = books.count() # is included with django automatically
+    avg_rating = books.aggregate(Avg('rating')) # need to import from django.db.models Avg; this returns a dictionary, not an int/str
     return render(request, 'book_outlet/index.html', {
-        'books': books # key to be used in template DTL syntax
+        'books': books, # key to be used in template DTL syntax
+        'total_number_of_books': num_books,
+        'average_rating': avg_rating
     })
 
 
-def book_detail(request, id):
+def book_detail(request, slug):
     # you can handle not found errors with try, except or easier w/get_or_404
     # try:
-    #     book = Book.objects.get(id=id) # get the book that matches the id in the request
+    #     book = Book.objects.get(slug=slug) # get the book that matches the id in the request
     # except:
     #     raise Http404()
-    book = get_object_or_404(Book, id=id)
+    book = get_object_or_404(Book, slug=slug)
     return render(request, 'book_outlet/book_detail.html', {
         'title': book.title,
         'author': book.author,
